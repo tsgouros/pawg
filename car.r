0bd0f47a3990b7011264be28dced0e997c019843
tsgouros <tsgouros@gmail.com>
2021-04-07 11:39:50 -0400

    ready for real pension flow instead of annuitized value

--- car.r
+++ car.r
@@ -102,7 +102,7 @@ doesMemberSeparate <- function(age, service, status, class="NONE") {
 
 doesMemberRetire <- function(age, service, status, class="NONE") {
     ## If already retired, get out.
-    if (status == "retired") return(status);
+    if ((status == "retired") | (status == "deceased")) return(status);
 
     if ((age >= 62) & (service >= 15)) {
         if ( ((age == 62) & (runif(1) > 0.4)) |
@@ -155,12 +155,24 @@ projectSalary <- function(age, service=1, class="NONE") {
 }
 
 projectPension <- function(salaryHistory) {
-    return( max(salaryHistory$salary) * 0.55);
-}
 
-## What's the annuitized cost of a pension at the moment of retirement?
-projectPensionCost <- function(pension, retireAge) {
-    return((77 - retireAge) * pension);
+    startingPension <- 14 * max(salaryHistory$salary) * 0.55;
+    cola <- 1.0;
+
+    ## This shouldn't be true ever, but if this person never retired,
+    ## send them away without a pension.
+    if (!("retired" %in% salaryHistory$status))
+        return(salaryHistory %>% mutate(pension = 0));
+
+    retireYear <- as.numeric(salaryHistory %>%
+                             filter(status=="retired") %>%
+                             summarize(retireYear=min(year)));
+
+    return(salaryHistory %>%
+           mutate(pension = ifelse(status == "retired",
+                                   startingPension * cola^(year - retireYear),
+                                   0)));
+
 }
 
 ## Accepts a salary history tibble and adds a column for the estimated
@@ -178,7 +190,7 @@ projectPremiums <- function(salaryHistory) {
 ## retirement, and backward to the initial hire.  Returns a tibble
 ## with salary figures for each working year, and a status column for
 ## active, separated, or retired.
-projectCareer <- function(year, age, service, salary, class="NONE") {
+projectCareer <- function(year, age, service, salary, class="NONE", verbose=FALSE) {
 
     salaries <- c(salary);
     ages <- c(age);
@@ -212,8 +224,11 @@ projectCareer <- function(year, age, service, salary, class="NONE") {
     currentStatus <- "active";
     currentService <- service + 1;
     for (iyear in seq(from = year + 1, to = year + (110 - age))) {
-        ##cat("projecting for", iyear, "\n");
+
         testAge <- age - (year - iyear);
+
+        if (verbose) cat (iyear, ": At age ", testAge,
+                          ", start ", currentStatus, sep="");
 
         ## Test for transitions.
         currentStatus <-
@@ -222,6 +237,8 @@ projectCareer <- function(year, age, service, salary, class="NONE") {
             doesMemberSeparate(testAge, currentService, currentStatus);
         currentStatus <-
             doesMemberRetire(testAge, currentService, currentStatus);
+
+        if (verbose) cat (", end ", currentStatus, ".\n", sep="");
 
         salaries <- c(salaries,
                       ifelse(currentStatus == "active",
@@ -281,9 +298,10 @@ member <- function(age=0, service=0, salary=0,
 
     ## If this member gets to retire, estimate pension.
     if ("retired" %in% salaryHistory$status) {
-        pension <- projectPension(salaryHistory);
+        salaryHistory <- projectPension(salaryHistory);
         retireYear <- as.numeric(salaryHistory %>%
             filter(status=="retired") %>% summarize(retireYear=min(year)));
+        pension <- 0;
     } else {
         retireYear <- NA;
         pension <- 0 ;
@@ -298,9 +316,8 @@ member <- function(age=0, service=0, salary=0,
 
     ## Estimate CAR for this employee.
     if (pension > 0) {
-        car <- findRate(salaryHistory,
-                        projectPensionCost(pension, max(salaryHistory$age)),
-                        flowName="premium");
+        car <- findRate(salaryHistory %>% mutate(netFlow = salary - pension),
+                        flowName="netFlow");
     } else {
         car <- NA;
     }
