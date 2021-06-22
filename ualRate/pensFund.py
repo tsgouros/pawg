bf5a6d1a9dce93668dd2a30813cc275d7a1720ac
TziJ <68706732+TziJ@users.noreply.github.com>
2021-06-22 17:48:30 -0400

    Structural updates to PensFund
    Reworked all functions, including __init__(). self.ledger now keeps track of individual asset values instead of total. annualReport() also reports asset values individually. makePayments() now has two inputs, and divides premiums/benefits among asset values based on __init__ percentages. Said percentages are now saved within self.pcts.

--- ualRate/pensFund.py
+++ ualRate/pensFund.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
+import numpy as np
 
 class pensFund(object):
 	def __init__(assetTotal, currYear, pctE = 0.6, pctB = 0.3, pctO = 0.1):
-		self.assets = assetTotal
 		self.currentYear = currYear
-		self.ledger = {self.currentYear : self.assets}
+		
+		self.pcts = [pctE, pctB, pctO]
     
-    	self.equity = pctE
-    	self.bonds = pctB
-    	self.other = pctO
+    	self.equity = self.pcts[0]*assetTotal
+    	self.bonds = self.pcts[1]*assetTotal
+    	self.other = self.pcts[2]*assetTotal
+		
+		self.ledger = {self.currentYear : [self.equity, self.bonds, self.other]}
 	
 	def annualReport(self, year):
 		if year in self.ledger:
-			return "Assets for the year " + year + " amount to $" + self.ledger[year] + "."
+			report = self.ledger[year]
+			return "Assets for the year " + year + ":\nEquity = $" + str(report[0]) + "\nBonds = $" + str(report[1]) + "\nOther = $" + str(report[2]) + "\nTotal: $" + str(sum(report))
 		else:
-			return "Asset total could not be found for the year " + year + "."
+			return "Assets could not be found for the year " + year + "."
 		
 	
-	def makePayments(self, premiums):
-		self.assets += premiums
-		self.ledger[self.currentYear] = self.assets
+	def makePayments(self, premiums, benefits):
+		self.equity += self.pcts[0]*premiums
+    	self.bonds += self.pcts[1]*premiums
+    	self.other += self.pcts[2]*premiums
+		
+		self.equity -= self.pcts[0]*benefits
+    	self.bonds -= self.pcts[1]*benefits
+    	self.other -= self.pcts[2]*benefits
 		
 	def addInvestmentEarnings(self, year):
-		self.currentYear = year ##maybe we could update the year somewhere else?
-		self.assets *= (1 + self.equity + self.bonds + self.other)
-		self.ledger[self.currentYear] = self.assets
+		e = np.random.normal(0.06, 0.03)
+		b = np.random.normal(0.04, 0.01)
+		o = np.random.normal(0.06, 0.05)
+		
+		self.equity *= (1+e)
+		self.bonds *= (1+b)
+		self.other *= (1+o)
+		
+		self.currentYear = year 
+		self.ledger[self.currentYear] = [self.equity, self.bonds, self.other]
 		
     
     
