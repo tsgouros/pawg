183cc7a923a8352bacbac543bf7ac85c19d727bd
TziJ <68706732+TziJ@users.noreply.github.com>
2021-07-13 12:19:52 -0400

    Model Running Functionality
    Added runModel() and getModelData() functions. 
    
    runModel does a 40-year simulation of pensPlan and returns the annual UAL, Assets, Liability, and UAL Growth Rates in a dictionary. 
    
    getModelData runs multiple models, collects the dictionaries from each model and returns them as a list, alongside a dictionary of the mean annual values of each metric.
    
    Both functions include code that will visualize the data in a graph that opens in a new tab, but is not saved anywhere at the moment.
    Both functions also include parameters for setting the volatility, contribution rate, and employment growth of the model(s).
    
    Also added variables for keeping track of the above metrics to pensPlan and pensPop.
    Discount Rate is now also a mutable input rather than a static value.

--- ualRate/pensPlan.py
+++ ualRate/pensPlan.py
@@ -2,31 +2,33 @@
 from pensPop import pensPop
 from pensFund import pensFund
 
+#modules for graphing data:
+import plotly.express as px
+import pandas as pd
+import statistics
+
 class pensPlan(object):
-    def __init__(self, N, currentYear):
+    def __init__(self, currentYear, volatility, employmentGrowth=1.0, contributionRate=1.0, discountRate=0.07):
 
         self.currentYear = currentYear
+        self.employ = employmentGrowth
+        self.cr = contributionRate
 
-        ## Discount rate choice probably belongs at the plan level
-        ##TJ: Need to check with Emily before setting this up ^
-        self.discountRate = 0.07
+        self.discountRate = discountRate
 
-        self.population = pensPop()
+        self.population = pensPop([], self.discountRate)
         self.population.simulatePopulation()
-        ## TJ: I'm using simulatePopulation for now, but in the end we should use simulateMembers in order to control the
-        ## size of the population
 
-        self.liability = self.population.calculateTotalLiability(self.population)
+        self.liability = self.population.calculateTotalLiability()
 
         ## Start off 3/4 funded.
-        self.fund = pensFund(0.75 * self.liability, self.currentYear)
-        ## TJ: Liability starts as zero, so the fund also starts with zero assets. If this
-        ## is not ideal, then maybe liability should have a hard-coded start value?
+        self.fund = pensFund(0.75 * self.liability, self.currentYear, volatility)
+        self.uaf = self.liability - sum(self.fund.ledger[self.currentYear])
+        self.assets = sum(self.fund.ledger[self.currentYear])
+        self.growthRate = 0.0
 
 
-    def annualReport(self, year=0):
-        """" TODO: Make this work for any year"""
-
+    def annualReport(self):
         #show number of active and retired members
         self.population.printReport()
         
@@ -37,33 +39,34 @@ class pensPlan(object):
         self.fund.printReport()
 
         #UAL
-        assets = self.fund.ledger[self.currentYear]
-        if self.liability - sum(assets) > 0:
-            print("Unfunded Liability: $%s" % max('{:,}'.format(round((self.liability - sum(assets)), 2))))
+        if self.uaf > 0:
+            print("Unfunded Liability: $%s (%s%%)" % ('{:,}'.format(round(self.uaf, 2)),
+                                                      round((self.uaf / self.liability) * 100)))
         else:
             print("Unfunded Liability: $0")
 
     def advanceOneYear(self):
         self.currentYear += 1
         info = self.population.advanceOneYear()
-        self.population.hireReplacements(info['replace'], 1.0)
-
-        #Calculate benefits owed this year (TODO)
-        print("Benefit payments amount to $%s" % '{:,}'.format(round(info['benefit'], 2)))
+        self.population.hireReplacements(info['replace'], self.employ)
 
         ## Calculate the increment of the normal cost.
-        newLiability = self.population.calculateTotalLiability(self.population)
-        premiums = newLiability - self.liability
+        newLiability = self.population.calculateTotalLiability()
+        premiums = (newLiability - self.liability) * self.cr
         self.liability = newLiability
-        ##TJ: updated self.liability here since it didn't change otherwise
-        print("Premium payments amount to $%s" % '{:,}'.format(round(premiums, 2)))
 
         self.fund.payPremiums(premiums)
         self.fund.payBenefits(info['benefit'])
         self.fund.addInvestmentEarnings(self.currentYear)
-
-
-
+        self.assets = sum(self.fund.ledger[self.currentYear])
+        if self.uaf !=0:
+            self.growthRate = ((max((self.liability - self.assets), 0) - self.uaf)/self.uaf)*100
+        else:
+            if self.liability - self.assets > 0:
+                self.growthRate = 100
+            else:
+                self.growthRate = 0
+        self.uaf = max((self.liability - self.assets), 0)
 
     def adjustEmployment(self, N):
         """Adjust employment up or down."""
@@ -72,14 +75,130 @@ class pensPlan(object):
         else:
             self.population.layoffMembers(-N)
 
+def runModel(volatility, employmentGrowth=1.0, contributionRate=1.0):
+    """TODO: Annotate with comments for Tom"""
+    #Dictionary to keep track of annual values
+    d = {}
+    p = pensPlan(2000, volatility, employmentGrowth, contributionRate)
+    d["UAF"] = [p.uaf]
+    d["Assets"] = [p.assets]
+    d["Liability"] = [p.liability]
+    d["Growth(%)"] = [round(p.growthRate, 2)]
+    d["Active Members"] = [sum([m.status == 'active' for m in p.population.members])]
+    d["Retired Members"] = [sum([m.status == 'retired' for m in p.population.members])]
+
+    #Run model for 40 years, saving data along the way
+    for year in range(40):
+        p.advanceOneYear()
+        d["UAF"].append(p.uaf)
+        d["Assets"].append(p.assets)
+        d["Liability"].append(p.liability)
+        d["Growth(%)"].append(round(p.growthRate, 2))
+        d["Active Members"].append(sum([m.status == 'active' for m in p.population.members]))
+        d["Retired Members"].append(sum([m.status == 'retired' for m in p.population.members]))
+
+    ##### Below should be commented out when using getModelData. Intended for isolated use.
+
+    #Convert dictionary data into a DataFrame, used to create the graph.
+    #df = pd.DataFrame(data=d, index=range(2000, 2041))
+
+    #Print the DataFrame, visible in console as a table.
+    #print(df)
+
+    #Create the graph itself, and open the visualization in a new tab. Does not save the graph anywhere at the moment.
+    #fig = px.line(df)
+    #fig.show()
+
+    return d
+
+def getModelData(size, vol, eg, cr):
+    #List to store the data from each run.
+    model_data = []
+    for i in range(size):
+        #Run the model, then append the resulting dictionary to the list.
+        model_data.append(runModel(vol, eg, cr))
+
+        #Console counter for me to see how far the script is progressing (it takes a long time!)
+        print(i, end=" ")
+
+    print("\nFinished running models. Averaging the data...")
+
+    ##Find the mean values across all runs and visualize them, to see overall shape of the data w/ the given parameters.
+    mean_data = {"UAF": [], "Assets": [], "Liability": [], "Growth(%)": [], "Active Members" : [], "Retired Members" : []}
+    for i in range(40):
+        m_uaf = [run["UAF"][i] for run in model_data]
+        m_assets = [run["Assets"][i] for run in model_data]
+        m_liability = [run["Liability"][i] for run in model_data]
+        m_growth = [run["Growth(%)"][i] for run in model_data]
+        m_active = [run["Active Members"][i] for run in model_data]
+        m_retired = [run["Retired Members"][i] for run in model_data]
+
+        uaf = statistics.mean(m_uaf)
+        assets = statistics.mean(m_assets)
+        liability = statistics.mean(m_liability)
+        growth = statistics.mean(m_growth)
+        active = statistics.mean(m_active)
+        retired = statistics.mean(m_retired)
+
+        mean_data["UAF"].append(uaf)
+        mean_data["Assets"].append(assets)
+        mean_data["Liability"].append(liability)
+        mean_data["Growth(%)"].append(round(growth, 2))
+        mean_data["Active Members"].append(active)
+        mean_data["Retired Members"].append(retired)
+
+    # Convert dictionary data into a DataFrame, used to create the graph.
+    df = pd.DataFrame(data=mean_data, index=range(2000, 2040))
+
+    # Print the DataFrame, visible in console as a table.
+    #print(df)
+
+    # Create the graph itself, and open the visualization in a new tab. Does not save the graph anywhere at the moment.
+    fig = px.line(df)
+    fig.show()
+    return [mean_data, model_data]
+
+
+
+
+
 
 if __name__ == "__main__":
-    plan = pensPlan(50, 2000)
-    print("Plan created!")
-    print("\nAnnual Report for %s:" % plan.currentYear)
-    plan.annualReport()
-    for i in range(50):
-        print("\nAdvancing a year...")
-        plan.advanceOneYear()
-        print("\nAnnual Report for %s:" % plan.currentYear)
-        plan.annualReport()
+
+    ##Default
+    vol = [0.04, 0.03, 0.02, 0.03, 0.04, 0.04]
+    eg = 1.0
+    cr = 1.0
+    getModelData(50, vol, eg, cr)
+
+    ##Test Impact of EG
+    vol = [0.04, 0.03, 0.02, 0.03, 0.04, 0.04]
+    eg = 1.25
+    cr = 1.0
+    getModelData(50, vol, eg, cr)
+
+    vol = [0.04, 0.03, 0.02, 0.03, 0.04, 0.04]
+    eg = 0.75
+    cr = 1.0
+    getModelData(50, vol, eg, cr)
+
+    ##Test impact of CR
+    vol = [0.04, 0.03, 0.02, 0.03, 0.04, 0.04]
+    eg = 1.0
+    cr = 1.25
+    getModelData(50, vol, eg, cr)
+
+    vol = [0.04, 0.03, 0.02, 0.03, 0.04, 0.04]
+    eg = 1.0
+    cr = 0.75
+    getModelData(50, vol, eg, cr)
+
+    #plan = pensPlan(2000)
+    #print("Plan created!")
+    #print("\nAnnual Report for %s:" % plan.currentYear)
+    #plan.annualReport()
+    #for i in range(50):
+        #print("\nAdvancing a year...")
+        #plan.advanceOneYear()
+        #print("\nAnnual Report for %s:" % plan.currentYear)
+        #plan.annualReport()
