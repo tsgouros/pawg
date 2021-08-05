f7e3332563c6cc59ab26ace7e5fc4bf2cae225b4
TziJ <68706732+TziJ@users.noreply.github.com>
2021-08-04 22:27:09 -0400

    payGo() and minor tweaks
    -payPremiums() renamed to addPremiums()
    -New payGo() function calculates and returns total asset overdraft after paying benefits, then resets any negative asset values to zero. This is to represent pay-as-you-go system where money is brought from the outside (the government) to cover costs.
    - payBenefits() now returns a call to payGo(), in order to pass the overdraft information to pensPlan.
    -Added precautionary code to payBenefits that avoids division by zero.

--- ualRate/pensFund.py
+++ ualRate/pensFund.py
@@ -30,7 +30,7 @@ class pensFund(object):
     def printReport(self):
         print(self.annualReport())
 
-    def payPremiums(self, premiums):
+    def addPremiums(self, premiums):
         total = self.equity + self.bonds + self.other
         if total == 0:
             self.equity += self.pcts[0] * premiums
@@ -41,20 +41,41 @@ class pensFund(object):
             self.bonds += (self.bonds / total) * premiums
             self.other += (self.other / total) * premiums
 
+    def payGo(self):
+        total = self.equity + self.bonds + self.other
+        if total >= 0:
+            return 0
+
+        total = 0
+        for asset in [self.equity, self.bonds, self.other]:
+            if asset < 0:
+                total -= asset
+
+        self.equity = max(self.equity, 0)
+        self.bonds = max(self.bonds, 0)
+        self.other = max(self.other, 0)
+
+        return total
+
     def payBenefits(self, benefits):
         total = self.equity + self.bonds + self.other
-        if total > 0:
+        if total == 0:
+            self.equity -= self.pcts[0] * benefits
+            self.bonds -= self.pcts[1] * benefits
+            self.other -= self.pcts[2] * benefits
+        else:
             self.equity -= (self.equity / total) * benefits
             self.bonds -= (self.bonds / total) * benefits
             self.other -= (self.other / total) * benefits
+        return self.payGo()
 
     def addInvestmentEarnings(self, year):
         e = np.random.normal(self.vol[0], self.vol[1])
         b = np.random.normal(self.vol[2], self.vol[3])
         o = np.random.normal(self.vol[4], self.vol[5])
-        #defaults: [0.06, 0.03, 0.04, 0.01, 0.06, 0.05]
+        # defaults: [0.06, 0.03, 0.04, 0.01, 0.06, 0.05]
 
-        #print("Investment Earnings amount to $%s" % '{:,}'.format(round(earnings, 2)))
+        # print("Investment Earnings amount to $%s" % '{:,}'.format(round(earnings, 2)))
 
         self.equity *= (1 + e)
         self.bonds *= (1 + b)
@@ -72,10 +93,11 @@ class pensFund(object):
 
 def testFund():
     print("Creating a fund. assetTotal = 200, currYear = 2020.\n")
-    fund = pensFund(200, 2020)
+    v = [0.06, 0.03, 0.04, 0.01, 0.06, 0.05]
+    fund = pensFund(200, 2020, v)
     print(fund.annualReport())
     print("\nAdvancing one year...\n")
-    fund.payPremiums(50)
+    fund.addPremiums(50)
     fund.payBenefits(100)
     fund.addInvestmentEarnings(2021)
     print(fund.annualReport(2021))
